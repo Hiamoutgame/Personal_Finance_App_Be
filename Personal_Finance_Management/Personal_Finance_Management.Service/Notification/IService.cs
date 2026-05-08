1fcdf72f974fef53a1b49097f485fa31433f95f2
Quinn3305 <kin332k5@gmail.com>
2026-05-08 08:21:13 +0700

    Note

---
+++ Personal_Finance_Management/Personal_Finance_Management.Service/Notification/IService.cs
@@ -0,0 +1,9 @@
+﻿namespace Personal_Finance_Management.Service.notification;
+
+public interface IService
+{
+    
+    Task<Response.GetNotificationsResponse> GetNotifications(string? type, string? status, int page, int pageSize);
+    
+    Task<Response.UpdateStatusResponse> UpdateStatus(Request.UpdateStatusRequest request);
+}
