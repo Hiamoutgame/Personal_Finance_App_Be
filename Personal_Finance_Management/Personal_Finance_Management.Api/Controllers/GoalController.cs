833e8382534c807879f70772dc947c764d5068d7
Quinn3305 <kin332k5@gmail.com>
2026-05-08 13:53:50 +0700

    full feature

--- Personal_Finance_Management/Personal_Finance_Management.Api/Controllers/GoalController.cs
+++ Personal_Finance_Management/Personal_Finance_Management.Api/Controllers/GoalController.cs
@@ -5,7 +5,7 @@ using Personal_Finance_Management.Service.goal;
 namespace Personal_Finance_Management.Api.Controllers;
 
 [ApiController]
-[Route("api/v1/goals")] 
+[Route("api/v1/goals")]
 [Authorize]
 public class GoalController : ControllerBase
 {
@@ -21,5 +21,33 @@ public class GoalController : ControllerBase
     {
         var result = await _goalService.GetGoals();
         return Ok(result);
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetGoalById(Guid id)
+    {
+        var result = await _goalService.GetGoalById(id);
+        return Ok(result);
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> CreateGoal([FromBody] Request.CreateGoalRequest request)
+    {
+        var result = await _goalService.CreateGoal(request);
+        return StatusCode(201, result);
+    }
+    
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> UpdateGoal(Guid id, [FromBody] Request.UpdateGoalRequest request)
+    {
+        var result = await _goalService.UpdateGoal(id, request);
+        return Ok(result);
+    }
+    
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteGoal(Guid id)
+    {
+        await _goalService.DeleteGoal(id);
+        return Ok(new { message = "Goal deleted" });
     }
 }
--- Personal_Finance_Management/Personal_Finance_Management.Service/Goal/IService.cs
+++ Personal_Finance_Management/Personal_Finance_Management.Service/Goal/IService.cs
@@ -1,10 +1,12 @@
-﻿using Personal_Finance_Management.Service.Goal;
+﻿
 
 namespace Personal_Finance_Management.Service.goal;
 
 public interface IService
 {
     public Task<Response.GetGoalsResponse> GetGoals();
-    
-    public Task<Response.GetGoalsResponse> GetGoalById(Guid id);
+    public Task<Response.GetGoalByIdResponse> GetGoalById(Guid id);
+    public Task<Response.CreateGoalResponse> CreateGoal(Request.CreateGoalRequest request);
+    public Task<Response.UpdateGoalResponse> UpdateGoal(Guid id, Request.UpdateGoalRequest request);
+    public Task DeleteGoal(Guid id);
 }
