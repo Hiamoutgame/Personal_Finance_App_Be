a4f83d0c8575e0a98aaf3ce83331d05aca6eaf46
Vo Viet Minh Duc <ducvvm4a@gmail.com>
2026-05-09 19:01:06 +0700

    minor

--- Personal_Finance_Management/Personal_Finance_Management.Service/Auth/Response.cs
+++ Personal_Finance_Management/Personal_Finance_Management.Service/Auth/Response.cs
@@ -9,6 +9,7 @@ public class Response
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
         public required string Email { get; set; }
+        public required string Role { get; set; }
         public required string AccessToken { get; set; }
     }
     public class LoginResponse
